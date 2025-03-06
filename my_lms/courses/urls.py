74c6e175999dd3f24f1d8cd52217110a06515c65
miplo <miplotnova@mail.ru>
2025-03-06 14:47:49 +0500

    Homework_30_1

--- my_lms/courses/urls.py
+++ my_lms/courses/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import CourseListView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CourseListView, LessonListCreateView, LessonRetrieveUpdateDestroyView
+
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
 
 
 urlpatterns = [
     path('courses/', CourseListView.as_view(), name='course-list'),
+    path('', include(router.urls)),
+    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
+    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
 ]
