76edf39a69b54c3390c5f78d26aed85d526ce8e5
miplo <miplotnova@mail.ru>
2025-03-07 17:13:38 +0500

    Homework_30_1

--- my_lms/courses/urls.py
+++ my_lms/courses/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseListView, LessonListCreateView, LessonRetrieveUpdateDestroyView
-
+from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet)
-
+router.register(r'courses', CourseViewSet)  # только через роутер
 
 urlpatterns = [
-    path('courses/', CourseListView.as_view(), name='course-list'),
-    path('', include(router.urls)),
-    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
-    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
+    path('', include(router.urls)),  # все эндпоинты для курса
+    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),  # эндпоинт для списка уроков
+    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),  # эндпоинт для конкретного урока
 ]
