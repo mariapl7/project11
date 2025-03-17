743727fd98a2f5a7933c6dd972f71a887aa5dc5d
miplo <miplotnova@mail.ru>
2025-03-17 11:43:24 +0500

    Homework_30_2

--- courses/models.py
+++ courses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django_filters import FilterSet, ModelChoiceFilter, ChoiceFilter, DateFromToRangeFilter
-from my_lms.users.models import CustomUser
+from users.models import CustomUser
 
 
 class Course(models.Model):
