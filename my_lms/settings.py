d2a7725c638a979a7c83d694edcb7a4bd3da0dfe
miplo <miplotnova@mail.ru>
2025-04-25 19:47:21 +0500

    Homework_34.2

--- my_lms/settings.py
+++ my_lms/settings.py
@@ -69,14 +69,21 @@ WSGI_APPLICATION = 'my_lms.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+if SERVER_TYPE == "local":
+    host_db = "127.0.0.1"
+else:
+    host_db = "db"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": host_db,
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
